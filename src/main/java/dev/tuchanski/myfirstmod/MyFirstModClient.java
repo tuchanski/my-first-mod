636a959d68f86b66d08b20ac793593832ce4943a
Tuchanski <gui.rocha2004@gmail.com>
2025-11-02 21:13:51 -0300

    chore: create project

---
+++ src/main/java/dev/tuchanski/myfirstmod/MyFirstModClient.java
@@ -0,0 +1,12 @@
+package dev.tuchanski.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
---
+++ src/main/java/dev/tuchanski/myfirstmod/MyFirstModDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.tuchanski.myfirstmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MyFirstModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
